c773b9f8994e09ae55c3c914ae0e0f89d9647743
Andrew Crites <explosion-pills@aysites.com>
2013-07-08 14:24:01 -0400

    Using UrlPieces observable array for URL parts

--- js/UrlsViewModel.js
+++ js/UrlsViewModel.js
@@ -1,11 +1,36 @@
-define('UrlsViewModel', ['jquery', 'knockout'], function ($, ko) {
+define('UrlsViewModel', ['jquery', 'knockout', 'UrlPiece'], function ($, ko, UrlPiece) {
     return function () {
         var self = this;
+        self.delay = 0;
         self.url = ko.observable();
 
         self.isValidUrl = ko.computed(function () {
             self.url();
             return document.getElementById('url').checkValidity();
+        });
+
+        self.pieces = ko.observableArray([
+            new UrlPiece('Scheme', 'protocol', self.url),
+            new UrlPiece('Host', 'hostname', self.url),
+        ]);
+
+        self.setUrl = ko.computed(function () {
+            var reverse = false;
+            self.url($("#url").val());
+            reverse = !(self.isValidUrl() && self.url());
+            if (reverse) {
+               self.pieces().reverse();
+            }
+            ko.utils.arrayForEach(self.pieces(), function (piece) {
+                if (piece.canSlideIn()) {
+                    piece.delay(self.delay);
+                    self.delay += 200;
+                }
+            });
+            if (reverse) {
+               self.pieces().reverse();
+            }
+            self.delay = 0;
         });
     }
 });
