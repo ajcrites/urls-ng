046d6175dbbe2db1e3ba4b46972d709d001f777a
Andrew Crites <acrites@mobiquityinc.com>
2013-10-16 16:24:44 -0400

    Removing no longer used library files

--- js/ko-custom-bindings/transition-delay.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'knockout'],
-function ($, ko) {
-    ko.bindingHandlers['transitionDelay'] = {
-        update: function (el, valueAccessor) {
-            $(el).css("transition-delay", valueAccessor());
-        },
-    };
-});
