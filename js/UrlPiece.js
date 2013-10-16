7b2cdb68e064a7179ace3ea32189dffe80f5a235
Andrew Crites <acrites@mobiquityinc.com>
2013-10-16 16:46:17 -0400

    Adding UrlPiece model file

---
+++ js/UrlPiece.js
@@ -0,0 +1,8 @@
+function UrlPiece(name, attr, url, definition) {
+    var self = this;
+    self.name = name;
+    self.attr = attr;
+    self.description = "";
+    self.url = url;
+    self.definition = definition;
+}
